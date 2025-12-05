de8cc51b981cdc96c41cdad53a7bbf3b6c650e36
killmepleaseahhh <ecoyst2k25@gmail.com>
2025-12-05 10:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function renderProducts(){
     card.className = 'card';
    card.innerHTML = `
   <div class="prod-img">
-    <img src="${p.image}" alt="${p.name}" />
+    <img src="${p.image}" alt="${p.name}" style="width:100%; height:100%; object-fit:cover; border-radius:8px;"/>
   </div>
 
   <div class="prod-name">${p.name}</div>
